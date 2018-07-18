9a7945e73b8ea4eb125d2f77c64337e4cdb0615a
HTML-Jeday <sergeykobzar33@gmail.com>
2018-07-18 11:18:25 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+const epxress = require('express');
+const app = epxress();
+const todos = require('./todos');
+
+
+app.get('/', (req, res) =>{
+    res.send('Express ')
+})
+
+app.get('/todos', (req, res) =>{
+
+    if(req.query.completed){
+        return res.json(todos.filter(todo => todo.completed.toString() === req.query.completed));
+    }
+
+    res.json(todos)
+    
+})
+app.get('/todos/:id', (req, res) =>{
+    let todo = todos.find(todo => todo.id == req.params.id)
+    
+    if(!todo) return res.status(404).send('Не найдено')
+
+    res.json(todo)
+})
+
+app.listen(1488, () => console.log('server is working'));
